1405ecf962b071e712d40a2a4bf885c58d2928b1
dddf1 <maman.r@gmail.com>
2022-01-29 16:03:26 +0200

    utils

--- Utils.py
+++ Utils.py
@@ -1,10 +1,11 @@
-import sys
-
-sys.path.append('/Users/Rafi/PycharmProjects/WorldOfGames/World-of-games/games')
-
-from MemoryGame import clear_screen
+import os
 
 SCORES_FILE_NAME = open("/Users/Rafi/PycharmProjects/WorldOfGames/World-of-games/Scores.txt", "w", encoding="utf-8")
 BAD_RETURN_CODE = 400
-Screen_cleaner = clear_screen()
 
+
+def screen_cleaner():
+    if os.name == 'posix':
+        _ = os.system('clear')
+    else:
+        _ = os.system('cls')
